eaaa20c9e5bfd458563c297a89a9518a780cf011
retree1 <70078437+retree1@users.noreply.github.com>
2024-05-01 14:54:59 -0400

    more info lol

--- chat-spammer-secret.lua
+++ chat-spammer-secret.lua
@@ -41,7 +41,7 @@ local function spamFunc(table, configName)
 		local message = table[math.random(1, #table)]
 
 		if secret.config_get("bOOC") then
-			RunConsoleCommand("say", "// " .. string.sub(message, 1, 126)) -- will add more options soon:tm: 
+			RunConsoleCommand("say", "// " .. string.sub(message, 1, 126)) -- will add more options soon:tm: (when i get a sub :point_right::point_left:) 
 		else
 			RunConsoleCommand("say", message)
 		end
